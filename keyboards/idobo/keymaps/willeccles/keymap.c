6d83b0dca6f44356805d171b87fa5d18991fa85b
Will Eccles <will@eccles.dev>
2020-11-04 09:50:42 -0500

    remove unnecessary code

--- keyboards/idobo/keymaps/willeccles/keymap.c
+++ keyboards/idobo/keymaps/willeccles/keymap.c
@@ -11,7 +11,6 @@ enum layernames {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* This layout is QWERTY because I know it */
     [LAYOUT_QWERTY] = LAYOUT_ortho_5x15(
             KC_GRV,   KC_1,   KC_2,     KC_3,     KC_4,   KC_5,    KC_MINS,  LWIN(KC_L),  KC_EQL,   KC_6,    KC_7,   KC_8,     KC_9,     KC_0,     KC_BSPC,
             KC_TAB,   KC_Q,   KC_W,     KC_E,     KC_R,   KC_T,    KC_P7,    KC_P8,       KC_P9,    KC_Y,    KC_U,   KC_I,     KC_O,     KC_P,     KC_DEL,
@@ -47,31 +46,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_NO,  KC_RSFT,  KC_RCTL,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,     RGB_SAD,  RGB_SAI,  KC_NO,
             KC_NO,  _______,  AG_NORM,  AG_SWAP,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  RGB_TOG,  RGB_RMOD,  RGB_VAD,  RGB_VAI,  RGB_MOD
             ),
-
-#if 0
-    /* This layout is Colemak because I don't know it */
-    [5] = LAYOUT_ortho_5x15(
-            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,
-            KC_PSCR,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-            KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE,  _______,  _______,  _______,
-            _______,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,  _______
-            ),
-    [6] = LAYOUT_ortho_5x15(
-            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,
-            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-            _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,  _______,  _______,  _______,
-            _______,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  KC_MPLY,  KC_MPLY,  KC_MPLY,  KC_MPLY,  _______,  _______,  _______,
-            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MPRV,  KC_VOLD,  KC_VOLU,  KC_MNXT,  _______,  _______,  _______
-            ),
-    [7] = LAYOUT_ortho_5x15(
-            KC_NO,  QWERTY,   COLEMAK,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,
-            KC_NO,  RESET,    DEBUG,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,     RGB_SPD,  RGB_SPI,  KC_NO,    KC_NO,  KC_NO,  KC_NO,
-            KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,     RGB_HUD,  RGB_HUI,  KC_NO,    KC_NO,  KC_NO,  KC_NO,
-            KC_NO,  KC_RSFT,  KC_RCTL,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,     RGB_SAD,  RGB_SAI,  KC_NO,    KC_NO,  KC_NO,  KC_NO,
-            KC_NO,  _______,  AG_NORM,  AG_SWAP,  KC_NO,  KC_NO,  KC_NO,  RGB_TOG,  RGB_RMOD,  RGB_VAD,  RGB_VAI,  RGB_MOD,  KC_NO,  KC_NO,  KC_NO
-            ),
-#endif
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
